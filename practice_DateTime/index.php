9382fef31788f8b7569d6c0aa0af0ce778052dd8
Ohno <k.ohno12.gt@gmail.com>
2019-06-19 22:08:54 +0900

    test

--- practice_DateTime/index.php
+++ practice_DateTime/index.php
@@ -1,29 +1,50 @@
 <?php 
 
-echo date(Y);
+// echo date(Y);
 
 $thisMonth;
 
 $thisMonth = new DateTime('first day of this month');
-var_dump($thisMonth);
+// var_dump($thisMonth);
 
-$dt = clone $thisMonth;
-var_dump($dt);
+// $dt = clone $thisMonth;
+// var_dump($dt);
 
-$dtn = $dt->modify('+1 month')->format('Y-m');
-var_dump($dtn);
+// $dtn = $dt->modify('+1 month')->format('Y-m');
+// var_dump($dtn);
 
-echo "\n";
+// echo "\n";
 
 
-$period = new DatePeriod(
+// $period = new DatePeriod(
+//     new DateTime('first day of this month'),
+//     new DateInterval(P1D),
+//     new DateTime('first day of next month')
+// );
+// var_dump($period);
+
+// foreach ($period as $day) {
+//         if($day->format('w') === '0') {
+//             echo "Sunday!";
+//         }
+//         echo "not Sunday";
+// }
+
+
+// echo $thisMonth->modify('-1 month')->format('Y-m-d');
+// echo $thisMonth->sub(new DateInterval('P1M'))->format('Y-m-d');
+
+// var_dump($thisMonth->modify('-1 day'));
+// var_dump(new DateInterval('P1D'));
+
+$period = new DatePeriod (
     new DateTime('first day of this month'),
-    new DateInterval(P1D),
-    new DateTime('first day of next month')
+    new DateInterval('P2D'),
+    new DateTime('last day of this month')
 );
-var_dump($period);
 
 foreach ($period as $day) {
-    if ()
+    var_dump($day);
     
 }
+
